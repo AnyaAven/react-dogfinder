e6569cdbd3df9e8d27b040e6cf8ac5ddde1c40a6
Anya Aven <leafeon96@gmail.com>
2024-06-03 18:17:29 -0400

    Finished

--- dogfinder/src/DogList.jsx
+++ dogfinder/src/DogList.jsx
@@ -1,5 +1,28 @@
-function DogList() {
-  return <div></div>;
+import DogDetails from "./DogDetails.jsx";
+
+/**
+ * Shows details of all the dogs on DogFinder
+ *
+ * Stateless
+ * props:
+ * dogs - [{  id: NUMBER,
+ *      name: STRING,
+ *      age: NUMBER,
+ *      facts: [STRING,...],
+ *      src: STRING
+ * },...]
+ */
+function DogList({ dogs }) {
+
+  return (
+  <div className="DogList">
+    {dogs.map(
+      dog =>
+        (
+          <DogDetails key={dog.id} dog={dog}/>
+        )
+    )}
+  </div>)
 }
 
 export default DogList;
