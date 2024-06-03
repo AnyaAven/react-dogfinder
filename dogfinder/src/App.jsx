207622913d1e464ee129a3402eb93e0c46796cbd
Anya Aven <leafeon96@gmail.com>
2024-06-03 18:16:48 -0400

    Added demo details

--- dogfinder/src/App.jsx
+++ dogfinder/src/App.jsx
@@ -1,9 +1,28 @@
 import './App.css';
+//import {v4 as uuid } from "uuid";
+
+import DogDetails from './DogDetails.jsx';
+
+const demoDog = {
+  "name": "Whiskey",
+  "age": 5,
+  "src": "whiskey",
+  "facts": [
+    "Whiskey loves eating popcorn.",
+    "Whiskey is a terrible guard dog.",
+    "Whiskey wants to cuddle with you!"
+  ],
+  "id": crypto.randomUUID()
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Good luck!</h1>
+      <h1>App!</h1>
+      <DogDetails
+       dog={demoDog}
+      >
+      </DogDetails>
     </div>
   );
 }
