3914cc55db13d1592f24c3a6a6a9db99b3a9722e
Anya Aven <leafeon96@gmail.com>
2024-06-03 18:16:35 -0400

    Added dogs prop

--- dogfinder/src/RoutesList.jsx
+++ dogfinder/src/RoutesList.jsx
@@ -1,10 +1,10 @@
 import DogDetails from "./DogDetails.jsx";
 import DogList from "./DogList.jsx";
 
-function RoutesList() {
+function RoutesList({ dogs }) {
   return (
     <Routes>
-      <Route path="/dogs" element={<DogList />} />
+      <Route path="/dogs" element={<DogList dogs={dogs} />} />
       <Route path="/dogs/:name" element={<DogDetails />} />
     </Routes>
   );
