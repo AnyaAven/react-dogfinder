ade11428f3b9302be1c09e5289d728f7d7b0e089
Anya Aven <leafeon96@gmail.com>
2024-06-03 19:59:53 -0400

    Added Nav comp

--- dogfinder/src/App.jsx
+++ dogfinder/src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
 //import {v4 as uuid } from "uuid";
 import RoutesList from "./RoutesList";
+import Nav from "./Nav.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { getDogs } from "./helper.js";
+import { getDogs, getDogNames } from "./helper.js";
 
 const DOGS = await getDogs();
+const DOG_NAMES = getDogNames(DOGS);
 
 function App() {
   return (
     <div className="App">
       <h1>App!</h1>
       <BrowserRouter>
+        <Nav links={DOG_NAMES} />
         <RoutesList dogs={DOGS} />
       </BrowserRouter>
     </div>
