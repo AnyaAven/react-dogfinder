a966734660e1aa7c6642e2f1c2a4b78a7275831e
Anya Aven <leafeon96@gmail.com>
2024-06-03 19:13:52 -0400

    Added everything check

--- dogfinder/src/RoutesList.jsx
+++ dogfinder/src/RoutesList.jsx
@@ -1,11 +1,17 @@
 import DogDetails from "./DogDetails.jsx";
 import DogList from "./DogList.jsx";
+import { Route, Routes } from "react-router-dom";
 
+/**
+ *
+ */
 function RoutesList({ dogs }) {
+
   return (
     <Routes>
       <Route path="/dogs" element={<DogList dogs={dogs} />} />
-      <Route path="/dogs/:name" element={<DogDetails />} />
+      <Route path="/dogs/:name" element={<DogDetails dogs={dogs}/>} />
+      <Route path="*" element={<h1>Not found! 404</h1>} />
     </Routes>
   );
   //TODO: chek DogDetails path
