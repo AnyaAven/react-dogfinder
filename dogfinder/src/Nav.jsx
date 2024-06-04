242ccda8f0f20878378d70c01a64918bebcd6337
Anya Aven <leafeon96@gmail.com>
2024-06-03 20:00:04 -0400

    Added Nav comp

---
+++ dogfinder/src/Nav.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+/**
+ * NavBar
+ * props:
+ * links - [str,...]
+ */
+function Nav({ links }) {
+
+  return (
+    <ul className="Nav">
+      <li><Link to="/dogs">Doggos</Link></li>
+      {links.map(
+        link => (
+          <li ><Link to={`/dogs/${link}`}>{link}</Link></li>
+        )
+      )}
+    </ul>
+  );
+}
+
+export default Nav;
